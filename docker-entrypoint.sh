c267dcd3cdd983b37eca1f98bc422f2ee7478693
yaoyao12138 <1539498396@qq.com>
2022-07-04 16:14:03 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o allexport
+
+
+if [[ "${FIPS_MODE}" = "true" ]]; then
+  echo "INFO: Running in FIPS approved-only mode (org.bouncycastle.fips.approved_only=true)"
+  KAFKA_JVM_PERFORMANCE_OPTS="${KAFKA_JVM_PERFORMANCE_OPTS} -Dorg.bouncycastle.fips.approved_only=true -Djava.security.properties=/etc/instana/kafka/java.security.bcfips"
+else
+  KAFKA_OPTS="-Dcom.redhat.fips=false ${KAFKA_OPTS}"
+fi
+
+set +o allexport
+
+main() {
+
+  echo "Starting Kafka..."
+  exec gosu kafka kafka-server-start.sh /etc/kafka/server.properties "$@"
+}
+
+main "$@"
